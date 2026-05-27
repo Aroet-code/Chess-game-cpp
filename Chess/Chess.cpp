24490217103ccfa8dbe1206c799d6826b53a8850
Aroet-code <andrey.pap.ru@yandex.ru>
2026-05-27 14:49:00 +0300

    modified:   Chess/Chess.cpp modified:   Chess/MovementHandler.h

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include "Board.h"
 #include "MovementHandler.h"
+#include "ChessNamespace.h"
+
+using namespace chess;
 
 int main()
 {
@@ -8,14 +11,20 @@ int main()
     Board* board = new Board(true);
     MovementHandler* mh = new MovementHandler(board);
 
-    board->white_rooks += ((uint64_t)8388608 << 12);
+    board->white_rooks ^= ((Bitboard)8388608 << 12);
+
+    board->black_pawns ^= ((Bitboard)1 << 51);
+    board->black_pawns ^= ((Bitboard)1 << 52);
+
+    board->white_pawns ^= ((Bitboard)1 << 11);
+    board->white_pawns ^= ((Bitboard)1 << 12);
     
     board->logBoard();
 
 
     std::cout << board->getPieceOnASquare(1, 1) << std::endl;
 
-    for (auto a : mh->getPossibleMoves(2, 1)) {
+    for (auto a : mh->getPossibleMoves(5, 1)) {
         std::cout << "x: " << a.x << " y: " << a.y << std::endl;
     }
 
